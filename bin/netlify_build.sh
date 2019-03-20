13dfc4b6deda2cce49bace3bb943cbe35cfcb7bf
Shandy Brown <github@ezide.com>
2019-03-20 15:17:51 -0700

    Delete table/. Instead, clone it from github during build

--- bin/netlify_build.sh
+++ bin/netlify_build.sh
@@ -5,5 +5,7 @@ echo "Beginning 1kFA Netlify Build Script"
 echo "my pwd is $PWD"
 echo "my home is $HOME"
 pwd
+git clone https://github.com/sjbrown/togetherness.git
+mv togetherness table
 
 echo "Finished! 1kFA Netlify Build Script"
