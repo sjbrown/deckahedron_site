9280ee54ffbe9f2a89efdf55f6dac0e237c4a5d2
Shandy Brown <github@ezide.com>
2019-03-20 15:53:03 -0700

    another attempt.  this time with testo/

--- bin/netlify_build.sh
+++ bin/netlify_build.sh
@@ -14,8 +14,12 @@ cp -a gm_playtest build/gm_playtest
 cp -a playtest_files build/playtest_files
 cp -a *.html build/
 
+mkdir testo
+echo "<h1>hello</h1>" > testo/index.html
+
 cd build
 git clone https://github.com/sjbrown/togetherness.git
+rm -rf table
 mv togetherness table
 
 echo "Finished! 1kFA Netlify Build Script"
