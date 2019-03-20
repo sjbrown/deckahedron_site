c456cb892c8174e738409e77b6031cbf34e6ba26
Shandy Brown <github@ezide.com>
2019-03-20 15:39:35 -0700

    try to build in the build/ directory

--- bin/netlify_build.sh
+++ bin/netlify_build.sh
@@ -5,6 +5,15 @@ echo "Beginning 1kFA Netlify Build Script"
 echo "my pwd is $PWD"
 echo "my home is $HOME"
 pwd
+
+mkdir build
+cp -a assets build/assets
+cp -a examples build/examples
+cp -a gm_playtest build/gm_playtest
+cp -a playtest_files build/playtest_files
+cp -a *.html build/
+
+cd build
 git clone https://github.com/sjbrown/togetherness.git
 mv togetherness table
 
